0da3d7ffcf2e27bfefe11e77922be3ee80c464bf
alexdiascr <git config --global user.email eng.alexdiasc@gmail.comgit config --global user.email>
2022-09-21 00:41:01 -0300

    fist commit

---
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,35 @@
+﻿using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        // validacao de notificacoes de erro
+
+        // validacao de modelstate
+
+        // validacao de operacao
+    }
+
+    [Route("api/fornecedores")]
+    public class FornecedoresController : MainController
+    {
+        private readonly IFornecedorRepository _fornecedorRepository;
+
+        public FornecedoresController(IFornecedorRepository fornecedorRepository)
+        {
+            _fornecedorRepository = fornecedorRepository;
+        }
+
+        public async Task<ActionResult<IEnumerable<FornecedorViewModel>>> ObterTodos()
+        {
+            var fornecedor = await _fornecedorRepository.ObterTodos();
+
+            return Ok(fornecedor);
+        }
+    }
+}
