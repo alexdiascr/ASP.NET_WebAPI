0eb8d848fea68e1155e37633b0a679d168e80a7b
alexdiascr <git config --global user.email eng.alexdiasc@gmail.comgit config --global user.email>
2023-03-03 00:39:33 -0300

    Publicando na azure

--- src/DevIO.Api/Program.cs
+++ src/DevIO.Api/Program.cs
@@ -4,6 +4,11 @@ using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
 
 // ConfigureServices
 builder.Services.AddDbContext<MeuDbContext>(optios =>
